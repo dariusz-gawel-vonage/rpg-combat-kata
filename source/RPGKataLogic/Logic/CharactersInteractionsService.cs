15d2d7aeaea80227abbed209c20b3198466b21d4
Dariusz Gaweł <dariusz.gawel@vonage.com>
2026-04-14 16:25:57 +0200

    Stage 2

--- source/RPGKataLogic/Logic/CharactersInteractionsService.cs
+++ source/RPGKataLogic/Logic/CharactersInteractionsService.cs
@@ -4,12 +4,15 @@ namespace RPGKataLogic.Logic;
 
 public static class CharactersInteractionsService
 {
-    public static void Damage(Character target, int damage)
+    public static void Damage(Character attacker, Character target, int damage)
     {
-        if(target.LiveState == LiveState.Dead)
+        if (target.LiveState == LiveState.Dead ||
+            attacker == target)
             return;
 
-        if(target.Health <= damage)
+        damage = CalculateFinalDamage(attacker, target, damage);
+
+        if (target.Health <= damage)
         {
             target.Health = 0;
             target.LiveState = LiveState.Dead;
@@ -20,11 +23,21 @@ public static class CharactersInteractionsService
         }
     }
 
-    public static void Heal(Character target, int healAmount)
+    public static void Heal(Character healer, Character target, int healAmount)
     {
-        if (target.LiveState == LiveState.Dead)
+        if (target.LiveState == LiveState.Dead || 
+            healer != target)
             return;
 
         target.Health = Math.Min(target.Health + healAmount, 1000);
+    }
+
+    private static int CalculateFinalDamage(Character attacker, Character target, int damage)
+    {
+        if (target.Level >= attacker.Level + 5)
+            damage = (int)(damage * 0.5);
+        else if (target.Level >= attacker.Level - 5)
+            damage = (int)(damage * 1.5);
+        return damage;
     }
 }
